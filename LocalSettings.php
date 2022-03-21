c18942208b5dcd2e132d515ba2e1e2a3ba1e67ae
klara_seitz <11996418+klaraseitz@users.noreply.github.com>
2022-03-21 14:49:34 +0100

    add own logo

--- LocalSettings.php
+++ LocalSettings.php
@@ -19,7 +19,7 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 ## Uncomment this to disable output compression
 # $wgDisableOutputCompression = true;
 
-$wgSitename = "All about crochet";
+$wgSitename = "So geht Häkeln";
 $wgMetaNamespace = "All_about_crochet";
 
 ## The URL base path to the directory containing the wiki;
@@ -38,10 +38,10 @@ $wgResourceBasePath = $wgScriptPath;
 ## The URL paths to the logo.  Make sure you change this from the default,
 ## or else you'll overwrite your logo when you upgrade!
 $wgLogos = [
-	'1x' => "$wgResourceBasePath/resources/assets/change-your-logo.svg",
+	'1x' => "$wgResourceBasePath/resources/assets/CrochetWikiLogoDE_135px.svg",
 
 
-	'icon' => "$wgResourceBasePath/resources/assets/change-your-logo.svg",
+	'icon' => "$wgResourceBasePath/resources/assets/CrochetWikiLogoDE_135px.svg",
 ];
 
 ## UPO means: this is also a user preference option
