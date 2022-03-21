f1899f941eaeabdcc5d23e8748d0fb04d51b3e9c
klara_seitz <11996418+klaraseitz@users.noreply.github.com>
2022-03-21 18:35:05 +0100

    remove unneeded skins

--- LocalSettings.php
+++ LocalSettings.php
@@ -132,8 +132,7 @@ wfLoadExtension( 'VisualEditor' );
 ## Default skin: you can change the default skin. Use the internal symbolic
 ## names, e.g. 'vector' or 'monobook':
 wfLoadSkin( 'chameleon' );
-wfLoadSkin( 'MinervaNeue' );
-$wgDefaultSkin = 'minerva';
+$wgDefaultSkin = 'chameleon';
 
 # End of automatically generated settings.
 # Add more configuration options below.
